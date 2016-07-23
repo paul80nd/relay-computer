20bbe28c82acb2688741dd6a24a17db76257da50
Paul Law <paullaw@MacBook-Air.local>
2016-07-23 07:30:02 +0100

    Clear Angular 2 build errors around es6 shim

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+/// <reference path="../typings/globals/es6-shim/index.d.ts" />
+
 import { bootstrap }    from "@angular/platform-browser-dynamic";
 import { AppComponent } from "./app/components/app.component";
 
