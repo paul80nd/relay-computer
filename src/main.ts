8fb98f70a88e198fb1e3c49efa45d6a2048bf505
Paul Law <paul@80nd.co.uk>
2017-05-16 19:34:29 +0100

    Upgrade to Angular 4

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,3 @@
-/// <reference path="../typings/globals/es6-shim/index.d.ts" />
-
 import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 import { AppModule } from "./app/components/app.module";
 
