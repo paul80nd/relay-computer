7fc1f4ce860f1302af365cb9820fbcfcf1b78e85
paullaw <paul@80nd.co.uk>
2019-12-02 17:02:20 +0000

    Drop old HttpModule use

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
@@ -25,7 +25,7 @@ import { ClarityModule } from '@clr/angular';
     BrowserAnimationsModule,
     ClarityModule,
     FormsModule,
-    HttpModule,
+    HttpClientModule,
     ArchitectureDiagramModule,
     BrowserModule,
     CardBaysModule,
