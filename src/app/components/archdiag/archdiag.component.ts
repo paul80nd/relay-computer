08056dc3c6b17347637d09a9364383811c85102c
paullaw <paul@80nd.co.uk>
2019-12-08 18:44:18 +0000

    Update arch/instrset components to be svg

--- src/app/components/archdiag/archdiag.component.ts
+++ src/app/components/archdiag/archdiag.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: '[rc-architecture-diagram]',
-    templateUrl: './archdiag.component.html'
+    templateUrl: './archdiag.component.svg'
 })
 
 export class ArchitectureDiagramComponent {
