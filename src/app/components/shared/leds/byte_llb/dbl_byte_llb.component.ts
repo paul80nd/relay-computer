cd391a053211b8ea5e2623f2a49ff7a11cc788e5
Paul Law <paul@80nd.co.uk>
2025-01-16 20:27:22 +0000

    ng control flow syntax

--- src/app/components/shared/leds/byte_llb/dbl_byte_llb.component.ts
+++ src/app/components/shared/leds/byte_llb/dbl_byte_llb.component.ts
@@ -1,14 +1,12 @@
 import { Component, Input } from '@angular/core';
 
 import { BitValue } from '@paul80nd/relay-computer-model';
-import { NgFor } from '@angular/common';
 
 /* eslint-disable @angular-eslint/component-selector -- used in svg group */
 @Component({
     selector: '[rc-dbl-byte-llb]',
     templateUrl: 'dbl_byte_llb.component.html',
-    standalone: true,
-    imports: [NgFor]
+    standalone: true
 })
 
 export class DoubleByteLedLightBarComponent {
